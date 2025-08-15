c75964ba17bc2ccd4c120b230c7bd47faf13d9c7
Deepanker Tiwari <deepamamazing@gmail.com>
2025-08-15 21:24:52 +0530

    Namaste Node- Season 2 ~ Diving into the APIs

--- src/app.js
+++ src/app.js
@@ -21,10 +21,10 @@ app.post("/signup", async (req, res) => {
   // const user=new User(userObj);
 
 
-  console.log(req.body);
+  // console.log(req.body);
   //When we use express.json and sending data from POST method
   const user = new User(req.body);
-  
+
   try {
     await user.save();
     res.send("User added successfully")
@@ -33,6 +33,64 @@ app.post("/signup", async (req, res) => {
   }
 })
 
+
+//Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong")
+  }
+})
+
+//Feed API- GET/feed- get all the user from the database
+app.get('/feed', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong")
+  }
+})
+
+//Delete a user from the database
+app.delete("/user", async (req, res) => {
+  //findById() in Mongoose is special — it only works for the _id field.
+  const userId = req.body.userId;
+
+  try {
+    // const user = await User.findByIdAndDelete({ _id: userId });
+    const user = await User.findByIdAndDelete(userId);
+    console.log(user);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong")
+  }
+})
+
+//Updating data in database
+app.patch("/user", async (req, res) => {
+  // const userId = req.body.userId;
+  const emailId = req.body.emailId;
+  const data = req.body;
+  // console.log(data);
+
+  try {
+    // const user=await User.findByIdAndUpdate({ _id: userId }, data,{returnDocument:"before"})
+    const user=await User.findOneAndUpdate({ emailId: emailId }, data,{returnDocument:"before"})
+    console.log(user);
+    res.send("User Updated Successfully")
+  } catch (err) {
+    res.status(400).send("Something went wrong")
+  }
+})
 
 connectDB()
   .then(() => {
