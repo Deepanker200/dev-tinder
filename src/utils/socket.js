5bf649d60df64800b7c8475bdc9d36356a0cebf0
Deepanker Tiwari <deepamamazing@gmail.com>
2025-09-09 23:51:24 +0530

    Namaste Nodejs~ Part-8 Socket.io chat feature added..

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,12 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const getSecretRoomId = (userId,targetUserId) => {
+    return crypto
+        .createHash("sha256")
+        .update([userId, targetUserId].sort().join("_"))
+        .digest("hex")
+}
 
 const initializeSocket = (server) => {
 
@@ -11,12 +19,19 @@ const initializeSocket = (server) => {
     io.on("connection", (socket) => {
         //Handle events
 
-        socket.on("joinChat", () => {
+        socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+            const roomId = getSecretRoomId(userId,targetUserId);
+            console.log(firstName + " joined the room: ", roomId);
+
+            socket.join(roomId);
 
         })
 
-        socket.on("sendMessage", () => {
+        socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
+            const roomId = getSecretRoomId(userId,targetUserId);
+            console.log(firstName + " " + text);
 
+            io.to(roomId).emit("messageReceived", { firstName, text });
         })
 
 
