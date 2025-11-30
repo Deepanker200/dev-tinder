fbaac15f1766145e4042e107c39dc08210dabea5
Deepanker Tiwari <deepamamazing@gmail.com>
2025-11-30 11:09:51 +0530

    Receiver name display using Chat API

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const { Chat } = require("../models/chat");
 const { userAuth } = require("../middlewares/auth");
+const User = require("../models/user");
 
 const chatRouter = express.Router();
 
 chatRouter.get('/chat/:targetUserId', userAuth, async (req, res) => {
-    const {targetUserId } = req.params;
+    const { targetUserId } = req.params;
 
-    const userId=req.user._id;
+    const userId = req.user._id;
     try {
-        let chat =await Chat.findOne({
+        let chat = await Chat.findOne({
             participants: {
                 $all: [userId, targetUserId]
             }
         }).populate({
-            path:"messages.senderId",       //It means: “Inside the messages array of this Chat document
-            select:"firstName lastName"
+            path: "messages.senderId",       //It means: “Inside the messages array of this Chat document
+            select: "firstName lastName"
         })
 
 
@@ -28,7 +29,12 @@ chatRouter.get('/chat/:targetUserId', userAuth, async (req, res) => {
             await chat.save();
         }
 
-        res.json(chat)
+
+        let connectionName = await User.findById(targetUserId, { _id: 0, firstName: 1 });
+        // console.log(targetUserId);
+
+
+        res.json({ chat, connectionName })
 
     } catch (err) {
         console.log(err);
